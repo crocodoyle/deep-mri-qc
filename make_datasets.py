00f2240c7a5c12f2870dd5af7dadd845067a05aa
Andrew Doyle <doyle.andrew@gmail.com>
2017-04-05 17:15:13 -0400

    downsampling t1s

--- make_datasets.py
+++ make_datasets.py
@@ -80,31 +80,63 @@ def make_nihpd(input_path, output_path, label_file):
   f = h5py.File(output_path + 'nihpd.hdf5', 'w')
 
 
-
-def train_test_val(val_frac, test_frac, labels):
-    sss_validation = StratifiedShuffleSplit(n_splits=1, test_size=val_frac + test_frac, random_state=42)
-    sss_test       = StratifiedShuffleSplit(n_splits=1, test_size=test_frac / (val_frac+test_frac), random_state=42)
-
-    train_indices, validation_indices, test_indices = None, None, None
-
-    for train_index, validation_index in sss_validation.split(np.zeros(len(labels)), labels):
-        train_indices      = train_index
-        validation_indices = validation_index
-
-    for validation_index, test_index in sss_test.split(np.zeros(len(labels[validation_indices])), labels[validation_indices]):
-        validation_indices = validation_index
-        test_indices       = test_index
-    print("training images:", len(train_index))
-    print("validation images:", len(validation_index))
-    print("test_index:", len(test_index))
-
-    return train_indices, validation_indices, test_indices
-
-
 def make_abide(path, label_file):
-
-
     patient_data = {}
+
+    index = 0
+    for filename in os.listdir(path + '/T1_downsampled/'):
+        patient_id = filename[:-4]
+
+        print(patient_id)
+        patient_data[patient_id]['index'] = index
+
+        index += 1
+
+    total_subjects = index
+
+
+    f = h5py.File(output_path + 'abide.hdf5', 'w')
+    f.create_dataset('images', (total_subjects, 181, 217, 181, 3), dtype='float32') # t1, gradient magnitude, surface distance
+    f.create_dataset('surfacepoints', (total_subjects, 40962*2, 3))
+    f.create_dataset('filenames', (total_subjects,), dtype=h5py.special_dtype(vlen=unicode))
+    f.create_dataset('labels', (total_subjects,), dtype='bool')
+
+
+    for filename in os.listdir(path + '/T1_downsampled/'):
+        patient_id = filename.split('+')[1]
+        i = patient_data[patient_id]['index']
+
+        img = nib.load(os.path.join(path + '/T1s/', filename)).get_data() # load image data
+
+        f['images'][i,:,:,:,0] = img
+
+
+        grad = np.gradient(img)
+
+        f['images'][i,:,:,:,1] = np.sum(grad, axis=0)
+
+        print(index, 'of', len(os.listdir(path + '/T1s/')))
+
+    for filename in os.listdir(path + '/surfaces/'):
+        patient_id = filename.split('+')[1]
+
+        i = patient_data[patient_id]['index']
+        surface_obj = open(path + '/surfaces/' + filename)
+        surface_obj.readlines(1) # first line is a header
+
+        print('patient', i)
+
+        for line in surface_obj.readlines():
+            coords = line.split(" ")
+            if len(coords) != 3:
+                break
+            f['surfacespoints'][i, j, :] = [float(coords[0]) + 72.5, float(coords[1]) + 126.5,
+                                            float(coords[2]) + 90.5]
+            j += 1
+
+        surface_obj.close()
+
+
 
     print("Reading QC labels...")
     label_file = open(os.path.join(path, label_file))
@@ -116,34 +148,13 @@ def make_abide(path, label_file):
         patient_id = line.split('+')[1].split('_')[0]
         label = int(line.split(',')[1])
 
-
-        anat_pos = line.find('anat')
-        if anat_pos > 0:
-            anat = line[anat_pos + 5]
-        else:
-            anat = "0"
-
-        followup_pos = line.find('followup')
-        if followup_pos > 0:
-            followup = line[followup_pos + 9]
-        else:
-            followup = "0"
-
-
-        patient_id += '-' + anat + '-' + followup
-
-        patient_data[patient_id] = {}
         patient_data[patient_id]['label'] = label
-        patient_data[patient_id]['index'] = i
+
+
 
     total_subjects = i
     print(total_subjects, "in QC file")
 
-    f = h5py.File(output_path + 'abide.hdf5', 'w')
-    f.create_dataset('images', (total_subjects+100, 361, 433, 361, 3), dtype='float32') # t1, gradient magnitude, surface distance
-    f.create_dataset('surfacepoints', (total_subjects+100, 40962*2, 3))
-    f.create_dataset('filenames', (total_subjects+100,), dtype=h5py.special_dtype(vlen=unicode))
-    f.create_dataset('labels', (total_subjects+100,), dtype='bool')
 
 
     #load labels
@@ -163,45 +174,7 @@ def make_abide(path, label_file):
     #load surface points
     for filename in os.listdir(path + '/surfaces/'):
 
-        patient_id = filename.split('+')[1]
 
-        anat_pos = filename.find('anat')
-        if anat_pos > 0:
-            anat = filename[anat_pos + 5]
-        else:
-            anat = "0"
-
-        followup_pos = filename.find('followup')
-        if followup_pos > 0:
-            followup = filename[followup_pos + 9]
-        else:
-            followup = "0"
-
-        patient_id += '-' + anat + '-' + followup
-
-
-        i = patient_data[patient_id]['index']
-
-
-        if "right" in filename:
-            patient_data[patient_id]['surfacefile-right'] = os.path.join(path + '/surfaces/', filename)
-            surface_obj = open(patient_data[patient_id]['surfacefile-right'])
-            j = 0
-        elif "left" in filename:
-            patient_data[patient_id]['surfacefile-left'] = os.path.join(path + '/surfaces/', filename)
-            surface_obj = open(patient_data[patient_id]['surfacefile-left'])
-            j = 40962
-
-
-        surface_obj.readlines(1)
-
-        for line in surface_obj.readlines():
-            coords = line.split(" ")
-            if len(coords) != 3:
-                break
-            f['surfacespoints'][i, j, :] = [float(coords[0]) + 72.25, float(coords[1]) + 126.25, float(coords[2]) + 90.25]
-            j += 1
-        surface_obj.close()
 
     #load T1, compute gradient image
     # for index, filename in enumerate(os.listdir(path + '/T1s/')):
@@ -268,9 +241,9 @@ def save_result(vol_info):
 
 
 def distance_to_surf(surface_points, index):
-    surface_distance = np.ones((361, 433, 361), dtype='float32')
+    surface_distance = np.ones((181, 217, 181), dtype='float32')
 
-    print("surface points: ", np.shape(surf_points))
+    print("surface points: ", np.shape(surface_points))
 
     floatX = np.zeros(np.shape(surface_distance)[0])
     floatY = np.zeros(np.shape(surface_distance)[1])
@@ -278,7 +251,7 @@ def distance_to_surf(surface_points, index):
 
 
     print("building KDTree...")
-    tree = KDTree(surf_points, leaf_size=10000)
+    tree = KDTree(surface_points, leaf_size=10000)
     print("built KDTree!")
 
     for z in range(np.shape(surface_distance)[0]):
