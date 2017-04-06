8d68bec388fcb3c151461d374e7382289550e958
Andrew Doyle <doyle.andrew@gmail.com>
2017-04-06 11:25:30 -0400

    compute surface distance

--- make_datasets.py
+++ make_datasets.py
@@ -14,7 +14,7 @@ from multiprocessing import Pool
 
 
 output_path = '/data1/data/ABIDE/'
-cores = 12
+cores = 4
 
 
 def make_ibis(input_path, output_path, label_file):
@@ -83,17 +83,13 @@ def make_nihpd(input_path, output_path, label_file):
 def make_abide(path, label_file):
     patient_data = {}
 
-    index = 0
-    for filename in os.listdir(path + '/T1_downsampled/'):
+    for index, filename in enumerate(os.listdir(path + '/T1_downsampled/')):
         patient_id = filename[:-4]
 
         print(patient_id)
         patient_data[patient_id]['index'] = index
 
-        index += 1
-
     total_subjects = index
-
 
     f = h5py.File(output_path + 'abide.hdf5', 'w')
     f.create_dataset('images', (total_subjects, 181, 217, 181, 3), dtype='float32') # t1, gradient magnitude, surface distance
@@ -102,21 +98,19 @@ def make_abide(path, label_file):
     f.create_dataset('labels', (total_subjects,), dtype='bool')
 
 
+    # load images and compute gradient
     for filename in os.listdir(path + '/T1_downsampled/'):
         patient_id = filename.split('+')[1]
+        print(patient_id)
+
         i = patient_data[patient_id]['index']
 
         img = nib.load(os.path.join(path + '/T1s/', filename)).get_data() # load image data
 
         f['images'][i,:,:,:,0] = img
+        f['images'][i,:,:,:,1] = np.sum(np.gradient(img), axis=0)
 
-
-        grad = np.gradient(img)
-
-        f['images'][i,:,:,:,1] = np.sum(grad, axis=0)
-
-        print(index, 'of', len(os.listdir(path + '/T1s/')))
-
+    # extract surfaces from combined left/right surface objs
     for filename in os.listdir(path + '/surfaces/'):
         patient_id = filename.split('+')[1]
 
@@ -126,112 +120,48 @@ def make_abide(path, label_file):
 
         print('patient', i)
 
-        for line in surface_obj.readlines():
+        for j, line in enumerate(surface_obj.readlines()):
             coords = line.split(" ")
             if len(coords) != 3:
                 break
             f['surfacespoints'][i, j, :] = [float(coords[0]) + 72.5, float(coords[1]) + 126.5,
                                             float(coords[2]) + 90.5]
-            j += 1
 
         surface_obj.close()
-
-
 
     print("Reading QC labels...")
     label_file = open(os.path.join(path, label_file))
     lines = label_file.readlines()
 
 
-    #establish a unique id as id number - scan-number - session-number
     for i, line in enumerate(lines[1:]):   # skip header
         patient_id = line.split('+')[1].split('_')[0]
         label = int(line.split(',')[1])
 
+        print('patient:', patient_id, 'label:', label)
+
         patient_data[patient_id]['label'] = label
-
-
-
-    total_subjects = i
-    print(total_subjects, "in QC file")
-
-
-
-    #load labels
-    num = 0
-    for patient_id in patient_data:
-        # print(patient_id)
-        i = int(patient_data[patient_id]['index'])
-        l = int(patient_data[patient_id]['label'])
-
-        f['labels'][i] = l
-
-        num += 1
-
-    print(num, 'patients in labels')
-
-
-    #load surface points
-    for filename in os.listdir(path + '/surfaces/'):
-
-
-
-    #load T1, compute gradient image
-    # for index, filename in enumerate(os.listdir(path + '/T1s/')):
-
-    #     patient_id = filename.split('+')[1]
-
-    #     anat_pos = filename.find('anat')
-    #     if anat_pos > 0:
-    #         anat = filename[anat_pos + 5]
-    #     else:
-    #         anat = "0"
-
-    #     followup_pos = filename.find('followup')
-    #     if followup_pos > 0:
-    #         followup = filename[followup_pos + 9]
-    #     else:
-    #         followup = "0"
-
-    #     patient_id += '-' + anat + '-' + followup
-    #     i = patient_data[patient_id]['index']
-
-    #     # print(i)
-    #     img = nib.load(os.path.join(path + '/T1s/', filename)).get_data()
-
-    #     # print('image shape:', np.shape(img))
-
-    #     f['images'][i,:,:,:,0] = img
-
-
-    #     grad = np.gradient(img)
-    #     # print('gradient shape:', np.shape(grad))
-
-    #     f['images'][i,:,:,:,1] = np.sum(grad, axis=0)
-
-    #     print(index, 'of', len(os.listdir(path + '/T1s/')))
 
     print("Computing surface distances... Could take a while")
 
-
-
+    # compute surface distance volumes in same space as T1
+    # p = Pool(cores)
     surf_points = np.zeros((40962*2, 3), dtype='float32')
 
-    p = Pool(cores)
-
     for i in range(total_subjects):
-        surf_points = f['surfacepoints'][i,:,:]
-        p.map(distance_to_surf, args=(surf_points, i,), callback = save_result)
+        surf_points = f['surfacepoints'][i, :, :]
 
-        print("Done ", str(i))
+        surface_distance_volume, output_filename = distance_to_surf(surf_points, patient_id)
+        nib.save(surface_distance_volume, os.path.join(output_path, output_filename))
 
-    p.close()
-    p.join()
+        # p.map(distance_to_surf, args=(surf_points, i,), callback = save_result)
 
+        print("Done ", str(i), 'of', total_subjects)
+
+    # p.close()
+    # p.join()
 
     f.close()
-
-
 
     return 0
 
@@ -240,7 +170,7 @@ def save_result(vol_info):
     nib.save(vol_info['surface'], os.path.join(output_path, str(vol_info['index']) + '.nii.gz'))
 
 
-def distance_to_surf(surface_points, index):
+def distance_to_surf(surface_points, patient_id):
     surface_distance = np.ones((181, 217, 181), dtype='float32')
 
     print("surface points: ", np.shape(surface_points))
@@ -258,21 +188,19 @@ def distance_to_surf(surface_points, index):
         print("z: ", z)
         for y in range(np.shape(surface_distance)[1]):
             for x in range(np.shape(surface_distance)[2]):
-                (distance, index) = tree.query(np.reshape([floatZ[z], floatY[y], floatX[x]], (1,3)), return_distance = True)
-                surface_distance[z,y,x] = distance
-                # brute force method, doesn't work very well
+                (distance, index) = tree.query(np.reshape([floatZ[z], floatY[y], floatX[x]], (1, 3)), return_distance = True)
+                surface_distance[z, y, x] = distance
+                # brute force method, very slow
                 # for point in surf_points:
                 #     d = euclidean([floatZ[z], floatY[y], floatX[x]], point)
 
                 #     if surface_distance[z,y,x] > d:
                 #         surface_distance[z,y,x] = d
 
-
     print('done ', filename)
-    result = {}
-    result['index'] = index
-    result['surface'] = surface_distance
-    return result
+
+    output_filename = patient_id + '_surface_distance.nii.gz'
+    return surface_distance, output_filename
 
 
 def combine_objs(obj1, obj2, newname):
@@ -296,16 +224,16 @@ if __name__ == "__main__":
     #
     #         combine_objs(os.path.join(path, 'surfaces/' + filename1), os.path.join(path, 'surfaces/' + filename2), os.path.join(path, 'surfaces/' + filename3))
 
-    for filename in os.listdir(path + '/T1s/'):
-        try:
-            patient_id = filename.split('+')[1]
+    # for filename in os.listdir(path + '/T1s/'):
+    #     try:
+    #         patient_id = filename.split('+')[1]
+    #
+    #         p = subprocess.Popen(['mincresample', '-nearest_neighbour', '-like', path + 'icbm_template_1.00mm.mnc', path + 'T1s/' + filename, path + 'T1_downsampled/' + patient_id + '.mnc', '-clobber'])
+    #         p.communicate()
+    #     except:
+    #         print filename
 
-            p = subprocess.Popen(['mincresample', '-nearest_neighbour', '-like', path + 'icbm_template_1.00mm.mnc', path + 'T1s/' + filename, path + 'T1_downsampled/' + patient_id + '.mnc', '-clobber'])
-            p.communicate()
-        except:
-            print filename
 
 
-
-    # make_abide('/data1/data/ABIDE/', 'labels.csv')
+    make_abide('/data1/data/ABIDE/', 'labels.csv')
   # make_nihpd('/data1/data/NIHPD/assembly/', 'data1/data/dl-datasets/')
