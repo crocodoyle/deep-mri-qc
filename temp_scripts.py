f3bfc3b763a308a274f62cc7e6cc3929c6929d86
Andrew Doyle <doyle.andrew@gmail.com>
2017-10-03 11:59:22 -0400

    Getting IBIS into better shape

--- temp_scripts.py
+++ temp_scripts.py
@@ -17,11 +17,23 @@ import nibabel as nib
 from multiprocessing import Pool, Process
 
 def flatten_IBIS():
-    for root, dirs, files in os.walk('/data1/data/IBIS/T1_Minc_Fail', topdown=False):
-        for file in files:
-            if '.mnc' in file:
-                print(root + file)
-                print(file.split('//')[-1])
+    with open(os.path.join('/data1/data/IBIS', 't1_qc.csv'), 'w') as label_file:
+        qc_writer = csv.writer(label_file)
+
+        for root, dirs, files in os.walk('/data1/data/IBIS/T1_Minc_Fail', topdown=False):
+            for file in files:
+                if '.mnc' in file:
+                    filename = file.split('//')[-1]
+                    qc_writer.writerow([filename, '0'])
+
+        for root, dirs, files in os.walk('/data1/data/IBIS/T1_Minc_Pass', topdown=False):
+            for file in files:
+                if '.mnc' in file:
+                    filename = file.split('//')[-1]
+                    qc_writer.writerow([filename, '2'])
+
+
+
 
 if __name__ == "__main__":
     flatten_IBIS()
