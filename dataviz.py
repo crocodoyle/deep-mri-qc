0c136d6336b4ff1744098a35c1d276271c457fa9
Andrew Doyle <doyle.andrew@gmail.com>
2017-10-06 11:55:25 -0400

    Visualizing data

---
+++ dataviz.py
@@ -0,0 +1,17 @@
+import h5py
+
+import matplotlib.pyplot as plt
+
+
+
+workdir = '/data1/data/deepqc/'
+
+if __name__ == '__main__':
+
+    f = h5py.File(workdir + 'deepqc.hdf5')
+
+    images = f['MRI']
+
+    for i, image in enumerate(images):
+        plt.imshow(image[96, ...])
+        plt.savefig(str(i) + '-' + slice + '.png')
