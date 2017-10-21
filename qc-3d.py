40c3db75d9cae2e3f3c4332918d1b07b361520d3
Andrew Doyle <doyle.andrew@gmail.com>
2017-10-21 11:31:33 -0400

    Finally training

--- qc-3d.py
+++ qc-3d.py
@@ -27,7 +27,7 @@ def qc_model():
 
     model = Sequential()
 
-    model.add(Conv3D(4, conv_size, activation='relu', strides=[1, 2, 2, 2, 1], input_shape=(image_size[0], image_size[1], image_size[2], 1)))
+    model.add(Conv3D(16, conv_size, activation='relu', strides=[2, 2, 2], input_shape=(image_size[0], image_size[1], image_size[2], 1)))
     # model.add(Dropout(0.2))
     # model.add(Conv3D(16, conv_size, activation='relu'))
     # model.add(Dropout(0.2))
@@ -36,19 +36,19 @@ def qc_model():
 
     # model.add(Conv3D(32, conv_size, activation='relu'))
     # model.add(Dropout(0.2))
-    model.add(Conv3D(8, conv_size, activation='relu'))
+    model.add(Conv3D(32, conv_size, activation='relu'))
     # model.add(Dropout(0.2))
     # model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=pool_size))
 
     # model.add(Conv3D(64, conv_size, activation='relu'))
     # model.add(Dropout(0.3))
-    model.add(Conv3D(16, conv_size, activation='relu'))
+    model.add(Conv3D(64, conv_size, activation='relu'))
     # model.add(Dropout(0.3))
     # model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=pool_size))
 #
-    model.add(Conv3D(32, conv_size, activation='relu'))
+    model.add(Conv3D(128, conv_size, activation='relu'))
     # model.add(Dropout(0.4))
     model.add(MaxPooling3D(pool_size=pool_size))
 
@@ -56,7 +56,7 @@ def qc_model():
     # # model.add(Dropout(0.4))
     # model.add(MaxPooling3D(pool_size=pool_size))
 
-    model.add(Conv3D(64, conv_size, activation='relu'))
+    model.add(Conv3D(256, conv_size, activation='relu'))
     model.add(Dropout(0.3))
 
     model.add(Flatten())
