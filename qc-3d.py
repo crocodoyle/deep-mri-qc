7bad20c5c0510547ef075ab432a936f9f63e3d13
Andrew Doyle <doyle.andrew@gmail.com>
2017-11-22 12:35:25 -0500

    3D reduce capacity

--- qc-3d.py
+++ qc-3d.py
@@ -32,15 +32,15 @@ def qc_model():
 
     model = Sequential()
 
-    model.add(Conv3D(16, conv_size, activation='relu', input_shape=(image_size[0], image_size[1], image_size[2], 1)))
+    model.add(Conv3D(4, conv_size, activation='relu', input_shape=(image_size[0], image_size[1], image_size[2], 1)))
     model.add(Dropout(0.2))
     model.add(BatchNormalization())
-    model.add(Conv3D(16, conv_size, activation='relu'))
+    model.add(Conv3D(8, conv_size, activation='relu'))
     model.add(Dropout(0.2))
     model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=pool_size))
 
-    model.add(Conv3D(32, conv_size, activation='relu'))
+    model.add(Conv3D(16, conv_size, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Conv3D(16, conv_size, activation='relu'))
     # model.add(Dropout(0.2))
