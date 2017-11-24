00b27dfd9c50dc46ed18c10a121d53f90d01d396
Andrew Doyle <doyle.andrew@gmail.com>
2017-11-24 13:55:04 -0500

    Fixing sens/spec

--- custom_loss.py
+++ custom_loss.py
@@ -100,13 +100,13 @@ def false_negatives(y_true, y_pred):
 
 def sensitivity(y_true, y_pred):
     """Return sensitivity (how many of the positives were detected?)"""
-    tp = true_positives(y_true, y_pred)
-    fn = false_negatives(y_true, y_pred)
+    tp = K.sum(true_positives(y_true, y_pred))
+    fn = K.sum(false_negatives(y_true, y_pred))
     return tp / (tp + fn + K.epsilon())
 
 
 def specificity(y_true, y_pred):
     """Return specificity (how many of the negatives were detected?)"""
-    tn = true_negatives(y_true, y_pred)
-    fp = false_positives(y_true, y_pred)
+    tn = K.sum(true_negatives(y_true, y_pred))
+    fp = K.sum(false_positives(y_true, y_pred)
     return tn / (tn+fp + K.epsilon())
