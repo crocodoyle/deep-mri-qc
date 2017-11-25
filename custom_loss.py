68b23db14cb672796815d1985b7e6cfd1cf27de8
Andrew Doyle <doyle.andrew@gmail.com>
2017-11-24 19:41:35 -0500

    tp tn fp fn

--- custom_loss.py
+++ custom_loss.py
@@ -64,21 +64,21 @@ def dice_np(im1, im2):
 
 def true_positives(y_true, y_pred):
     """Return number of true positives"""
-    return K.round(y_pred) *y_true
+    return K.argmax(y_true) * K.argmax(y_pred)
 
 def true_negatives(y_true, y_pred):
     """Return number of true negatives"""
-    return (1 - K.round(y_pred)) * (1 - y_true)
+    return (1 - K.argmax(y_pred)) * (1 - K.argmax(y_true))
 
 
 def false_positives(y_true, y_pred):
     """Return number of false positives"""
-    return (K.round(y_pred)) * (1 - y_true)
+    return (K.argmax(y_pred)) * (1 - K.argmax(y_true))
 
 
 def false_negatives(y_true, y_pred):
     """Return number of false negatives"""
-    return (1 - K.round(y_pred)) * (y_true)
+    return (1 - K.argmax(y_pred)) * (K.argmax(y_true))
 
 
 def sensitivity(y_true, y_pred):
