b53a4a17800c9c5e0190e08609d97d0a63895ce7
Andrew Doyle <doyle.andrew@gmail.com>
2017-11-27 11:59:32 -0500

    Fixing production script

--- t1qc.py
+++ t1qc.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from keras.models import load_model
 
-from dltk.core.io.preprocessing import normalise_zero_one, resize_image_with_crop_or_pad
+# from dltk.core.io.preprocessing import normalise_zero_one, resize_image_with_crop_or_pad
 
 import keras.backend as K
 # from vis.utils import utils
@@ -20,40 +20,39 @@ if __name__ == "__main__":
     image_file = args.t1image
     print('Input image file:', image_file)
 
-
     # K.set_image_dim_ordering('th')
 
     try:
         img = nib.load(image_file).get_data()
 
-        img = resize_image_with_crop_or_pad(img, (168, 256, 244), mode='constant')
-        img = normalise_zero_one(img)
+        # img = resize_image_with_crop_or_pad(img, (168, 256, 244), mode='constant')
+        # img = normalise_zero_one(img)
 
-        # (x_size, y_size, z_size) = img.shape
-        # # print('original image size:', img.shape)
-        #
-        # y_max, z_max = 256, 224
-        #
-        # while y_size < y_max or z_size < z_max:
-        #     img = np.pad(img, 1, 'constant')
-        #     (x_size, y_size, z_size) = img.shape
-        #
-        # y_mid, z_mid = y_size/2, z_size/2
-        #
-        # x_slice = int(x_size/2)
-        # y_start, y_stop = int(y_mid-y_max/2), int(y_mid+y_max/2)
-        # z_start, z_stop = int(z_mid-z_max/2), int(z_mid+z_max/2)
-        #
+        (x_size, y_size, z_size) = img.shape
+        # print('original image size:', img.shape)
+
+        y_max, z_max = 256, 224
+
+        while y_size < y_max or z_size < z_max:
+            img = np.pad(img, 1, 'constant')
+            (x_size, y_size, z_size) = img.shape
+
+        y_mid, z_mid = y_size/2, z_size/2
+
+        x_slice = int(x_size/2)
+        y_start, y_stop = int(y_mid-y_max/2), int(y_mid+y_max/2)
+        z_start, z_stop = int(z_mid-z_max/2), int(z_mid+z_max/2)
+
         # print('x_slice:', x_slice)
         # print('y_start, y_stop:', y_start, y_stop)
         # print('z_start, z_stop:', z_start, z_stop)
-        #
-        #
-        # img_slice = img[x_slice, y_start:y_stop, z_start:z_stop][np.newaxis, np.newaxis, ...]
-        #
-        # print(img_slice.shape)
 
-        model = load_model('~/ibis-qc.hdf5')
+
+        img_slice = img[x_slice, y_start:y_stop, z_start:z_stop][np.newaxis, np.newaxis, ...]
+
+        print(img_slice.shape)
+
+        model = load_model('~/IBIS-DeepMRIQC-model-v0.hdf5')
 
         slice_predictions = []
         for i in range(10):
@@ -65,4 +64,3 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(e)
-
