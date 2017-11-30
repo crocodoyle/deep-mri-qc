fef2a43cd91367e626eba4b4dbfb172dce16f93f
Andrew Doyle <doyle.andrew@gmail.com>
2017-11-29 20:00:05 -0500

    Saving figure

--- qc-ibis-2d.py
+++ qc-ibis-2d.py
@@ -466,7 +466,7 @@ if __name__ == "__main__":
 
         model_checkpoint = ModelCheckpoint(results_dir + "best_weights" + "_fold_" + str(k) + ".hdf5", monitor="val_acc", verbose=0, save_best_only=True, save_weights_only=False, mode='max')
 
-        hist = model.fit_generator(batch(train_indices, batch_size, True), np.ceil(len(train_indices)/batch_size), epochs=100, validation_data=batch(validation_indices, batch_size), validation_steps=np.ceil(len(validation_indices)//batch_size), callbacks=[model_checkpoint], class_weight = {0:5, 1:1})
+        hist = model.fit_generator(batch(train_indices, batch_size, True), np.ceil(len(train_indices)/batch_size), epochs=400, validation_data=batch(validation_indices, batch_size), validation_steps=np.ceil(len(validation_indices)//batch_size), callbacks=[model_checkpoint], class_weight = {0:5, 1:1})
 
         model.load_weights(results_dir + "best_weights" + "_fold_" + str(k) + ".hdf5")
         model.save(results_dir + 'ibis_qc_model' + str(k) + '.hdf5')
@@ -522,6 +522,9 @@ if __name__ == "__main__":
 
     plt.xlabel('Metric')
     plt.ylabel('Value')
+
+    results_dir = workdir + '/experiment-' + str(experiment_number) + '/'
+    plt.savefig(results_dir + 'metrics_boxplot.png')
 
     print(scores)
 
