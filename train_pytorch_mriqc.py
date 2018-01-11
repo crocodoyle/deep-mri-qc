75eb2c5cdacb357a1d27c03fe00e5e50754fe0ca
Andrew Doyle <doyle.andrew@gmail.com>
2018-01-11 15:37:50 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -60,7 +60,7 @@ class QCDataset(Dataset):
         good_index = self.indices[index]
 
         image = self.images[good_index, ...][np.newaxis, :, :, 192//2]
-        label = torch.from_numpy(self.labels[good_index, ...])
+        label = torch.from_numpy(np.argmax(self.labels[good_index, ...]), dytype='int')
 
         return image, label
 
