6d261f36c416e578422758d999bdbe501cdcf076
Andrew Doyle <doyle.andrew@gmail.com>
2018-01-11 16:13:56 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -52,7 +52,6 @@ class QCDataset(Dataset):
         self.n_subjects = len(all_indices)
         self.indices = np.zeros((self.n_subjects))
 
-        good_subject_index = 0
         for i, index in enumerate(all_indices):
             self.indices[i] = index
 
@@ -74,17 +73,19 @@ ds030_indices = pickle.load(open(workdir + 'ds030_indices.pkl', 'rb'))
 ibis_indices = pickle.load(open(workdir + 'ibis_indices.pkl', 'rb'))
 ping_indices = pickle.load(open(workdir + 'ping_indices.pkl', 'rb'))
 
-all_indices = abide_indices + ds030_indices + ibis_indices + ping_indices
-qc_dataset = QCDataset(workdir + 'deepqc-all-sets.hdf5', all_indices)
+train_indices = abide_indices + ds030_indices + ibis_indices + ping_indices
+
+train_dataset = QCDataset(workdir + 'deepqc-all-sets.hdf5', train_indices)
+test_dataset = QCDataset(workdir + 'deepqc-all-sets.hdf5', ds030_indices)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
-    qc_dataset,
+    train_dataset,
     batch_size=args.batch_size, **kwargs)
 
-# test_loader = torch.utils.data.DataLoader(
-#     qc_dataset,
-#     batch_size=args.test_batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=args.batch_size, **kwargs)
 
 class Net(nn.Module):
     def __init__(self):
@@ -133,25 +134,27 @@ def train(epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss_val.data[0]))
 
-# def test():
-#     model.eval()
-#     test_loss = 0
-#     correct = 0
-#     for data, target in test_loader:
-#         if args.cuda:
-#             data, target = data.cuda(), target.cuda()
-#         data, target = Variable(data, volatile=True), Variable(target)
-#         output = model(data)
-#         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-#         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-#         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-#
-#     test_loss /= len(test_loader.dataset)
-#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-#         test_loss, correct, len(test_loader.dataset),
-#         100. * correct / len(test_loader.dataset)))
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        loss_function = nn.CrossEntropyLoss()
+
+        test_loss += loss_function(output, target, size_average=False).data[0] # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 
 for epoch in range(1, args.epochs + 1):
     train(epoch)
-    # test()
+    test()
