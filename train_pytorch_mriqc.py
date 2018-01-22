fa1627366581b3d271896716b61283af79f27b98
Andrew Doyle <doyle.andrew@gmail.com>
2018-01-22 17:48:35 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -214,7 +214,7 @@ def example_pass_fails(results_dir):
 
             plt.close()
             plt.imshow(image_batch[i, 0, :, :], cmap='gray')
-            plt.axes('off')
+            plt.axis('off')
             plt.savefig(results_dir + '/imgs/' + qc_decision + '_batch_' + str(batch_idx) + '_img_' + str(i) + '.png', bbox_inches='tight')
 
 
