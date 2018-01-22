f2603cf87b3d96b348cfa2ec395b4e1935ba4ade
Andrew Doyle <doyle.andrew@gmail.com>
2018-01-22 12:41:02 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -116,10 +116,10 @@ optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        class_weight = [1.0, 0.001]
+        class_weight = torch.FloatTensor([1.0, 0.001])
         if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target, class_weight = Variable(data), Variable(target).type(torch.LongTensor), Variable(class_weight).type(torch.FloatTensor)
+            data, target, class_weight = data.cuda(), target.cuda(), class_weight.cuda()
+        data, target, class_weight = Variable(data), Variable(target).type(torch.LongTensor), Variable(class_weight)
         optimizer.zero_grad()
         output = model(data)
         loss = nn.CrossEntropyLoss(weight=class_weight)
