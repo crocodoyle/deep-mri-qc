50b004f9edef892a781452a427e3598ec5916b7c
Andrew Doyle <doyle.andrew@gmail.com>
2018-02-22 10:46:35 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -378,10 +378,10 @@ def example_pass_fails(model, train_loader, test_loader, results_dir, grad_cam):
             lw = 4
         else:
             lw = 2
-        axes[0].plot(bins[:-1], histograms[site], lw=lw, label=site)
+        axes.plot(bins[:-1], histograms[site], lw=lw, label=site)
 
-    axes[0].set_title('histogram of grey values')
-    axes[0].set_ylabel('# voxels')
+    axes.set_title('histogram of grey values')
+    axes.set_ylabel('# voxels')
 
     plt.legend(shadow=True)
     plt.tight_layout()
