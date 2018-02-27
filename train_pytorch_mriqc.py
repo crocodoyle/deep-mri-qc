4ae3d777210121ab04b792bcb0bb2c2922b83739
Andrew Doyle <doyle.andrew@gmail.com>
2018-02-27 11:16:04 -0500

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -26,12 +26,12 @@ import matplotlib.cm as cm
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch DeepMRIQC training.')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 16)')
+                    help='input batch size for training (default: 64)')
 parser.add_argument('--val-batch-size', type=int, default=8, metavar='N', help='input batch size for validation (default: 8')
 parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for testing (default: 16)')
-parser.add_argument('--epochs', type=int, default=25, metavar='N',
-                    help='number of epochs to train (default: 10)')
+                    help='input batch size for testing (default: 64)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
 parser.add_argument('--folds', type=int, default=10, metavar='N',
                     help='number of folds to cross-validate over (default: 10)')
 parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
@@ -332,9 +332,10 @@ def example_pass_fails(model, train_loader, test_loader, results_dir, grad_cam):
                 plt.close()
                 plt.imshow(image_batch[i, 0, :, :], cmap='gray', origin='lower')
                 plt.axis('off')
-                plt.savefig(results_dir + '/imgs/' + qc_decision + '_batch_' + str(batch_idx) + '_img_' + str(i) + '.png', bbox_inches='tight')
+                filename = results_dir + '/imgs/' + qc_decision + '_' + site + '_' + str(batch_idx) + '_img_' + str(i) + '.png'
+                plt.savefig(filename, bbox_inches='tight')
         except IndexError as e:
-            pass
+            print('Couldnt save one file')
 
     for batch_idx, (data, target, sites) in enumerate(test_loader):
         if args.cuda:
