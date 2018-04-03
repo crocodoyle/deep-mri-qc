c2a64a38b3601aff6aa174fd57c2c91790df6050
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-03 14:11:39 -0400

    bug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -503,7 +503,7 @@ if __name__ == '__main__':
     grad_cam = GradCam(model = model, target_layer_names=['output'], use_cuda=args.cuda)
     # example_pass_fails(model, train_loader, test_loader, results_dir, grad_cam)
 
-    dummy_input = Variable(torch.randn(1, 192, 256, 1))
+    dummy_input = Variable(torch.randn(1, 1, 192, 256))
 
     input_names = ["coronal_slice"]
     output_names = ["pass_fail"]
