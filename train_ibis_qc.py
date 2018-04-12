799ae001f28d30221eed05658932840f8e11283b
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-12 14:47:46 -0400

    bug

--- train_ibis_qc.py
+++ train_ibis_qc.py
@@ -320,7 +320,7 @@ if __name__ == '__main__':
     n_fail = len(ibis_indices) - n_pass
 
     print('Whole dataset has ' + str(len(ibis_indices)) + ' images ('+ str(n_pass) + ' PASS, ' + str(n_fail) + ' FAIL)')
-    fail_weight = n_pass / n_total
+    fail_weight = (n_pass / n_total) * 10
     pass_weight = n_fail / n_total
     print('Setting class weighting to ' + str(fail_weight) + ' for FAIL class and ' + str(
         pass_weight) + ' for PASS class')
