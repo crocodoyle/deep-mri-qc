62f7efe4d7c06295bee763ebbb890f84f1361f01
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-11-13 13:55:32 -0500

    debug

--- qc_pytorch_models.py
+++ qc_pytorch_models.py
@@ -168,7 +168,7 @@ class ModelWithBagDistribution(nn.Module):
     """
     def __init__(self, model, n_slices):
         super(ModelWithBagDistribution, self).__init__()
-        self.slice_model = nn.Module(model)
+        self.slice_model = nn.Sequential(model)
 
         self.n_slices = n_slices
 
