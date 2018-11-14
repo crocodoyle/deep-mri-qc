9e9637cb22b9a9e29a4c8a0fef9d86df89a4f54c
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-11-14 12:14:49 -0500

    debug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -219,7 +219,7 @@ def learn_bag_distribution(f, f2, train_indices, validation_indices, test_indice
             print('output', output.shape)
             print('single output:', output[0:0, :])
             loss = nn.CrossEntropyLoss()
-            loss_val = loss(output[0:0, :], target)
+            loss_val = loss(output, target)
             loss_val *= sample_weight
             loss_val.backward()
 
