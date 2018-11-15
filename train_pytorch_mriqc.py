efc35052756b93e42ca96309ac3375dee7730f63
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-11-15 15:01:20 -0500

    debug

--- train_pytorch_mriqc.py
+++ train_pytorch_mriqc.py
@@ -96,7 +96,7 @@ class AllSlicesQCDataset(Dataset):
         label_confidence = self.confidence[good_index]
         image_slices = torch.FloatTensor(self.images[good_index, 0, image_shape[0] // 2 - n_slices : image_shape[0] // 2 + n_slices, :, :])
 
-        return image_slices, label, label_confidence
+        return image_slices, int(label), label_confidence
 
     def __len__(self):
         return self.n_subjects
